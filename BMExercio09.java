bcfc3119324fb120c00cd7fc9b04d9ee51d8adcc
lyzzandro <lyzzandro91@gmail.com>
2022-05-30 18:54:52 -0300

    enviando questao 9

---
+++ BMExercio09.java
@@ -0,0 +1,11 @@
+public class BMExercio09 {
+    public static void main(String[] args) {
+        int numero = 10;
+        int contador = 20;
+
+        while (contador > 0) {
+            System.out.print(numero + "  ");
+            contador--;
+        }
+    }
+}
