fb5eaef5dda64ae77bedb155b179d65fab46be97
João Alfredo Alves <68473607+JoaoAlfredoAlves@users.noreply.github.com>
2022-06-03 15:22:32 -0300

    adicionando comentários com o enunciado das questões
    adicionando comentários em cada questão, detalhando seu enunciado, ou seja, o que cada questão pedia em seu programa de resolução.

--- BMExercicio07.java
+++ BMExercicio07.java
@@ -1,3 +1,14 @@
+
+/**
+ * 7. Escreva um programa que, dada uma variável x (com valor 180, por exemplo), temos y de acordo com a seguinte regra: 
+ * se x é par, y = x / 2
+ * se x é impar, y = 3 * x + 1
+ * imprime y
+ * O programa deve então jogar o valor de y em x e continuar até que y
+ * tenha o valor final de 1.
+ * Por exemplo, para x = 13, a saída será:
+ * 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
+ */
 import java.util.Scanner;
 
 public class BMExercicio07 {
