a3da95d0606c3ff5c78d1d74057a5f542af410c9
João Alfredo Alves <68473607+JoaoAlfredoAlves@users.noreply.github.com>
2022-06-03 14:37:58 -0300

    enviando atividade prática 4 CBB

---
+++ CBBAtividadePratica4.java
@@ -0,0 +1,14 @@
+/**
+ * Atividade prática 4
+ * Faça um programa que imprima todos os números de 0 a 100.
+ */
+public class CBBAtividadePratica4 {
+    public static void main(String[] args) {
+        int numero = 0;
+
+        while (numero <= 100) {
+            System.out.println(numero);
+            numero++;
+        }
+    }
+}
