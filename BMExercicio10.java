2c20fcb3ac3bca046808dfee1492324ae772037e
João Alfredo Alves <68473607+JoaoAlfredoAlves@users.noreply.github.com>
2022-06-03 15:08:44 -0300

    enviando questao 10 BM

---
+++ BMExercicio10.java
@@ -0,0 +1,14 @@
+/**
+ * 10. Reescreva o programa anterior utilizando o comando do-while.
+ */
+public class BMExercicio10 {
+    public static void main(String[] args) {
+        int numero = 10;
+        int contador = 20;
+
+        do {
+            System.out.print(numero + "  ");
+            contador--;
+        } while (contador > 0);
+    }
+}
